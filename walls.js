19d324653c8284194f42129e935294f5163acb3a
tunghoang176 <66507974+tunghoang176@users.noreply.github.com>
2020-06-20 04:22:12 +1000

    Add files via upload
    please add 3 code lines
     <script type="application/javascript" src="./src/walls.js"></script> to index.html
     createWalls(); 
    resetWall(); to index.js 
    please

---
+++ walls.js
@@ -0,0 +1,150 @@
+"use strict";
+
+Physijs.scripts.worker = "js/physijs_worker.js";
+Physijs.scripts.ammo = "ammo.js";
+
+var block_material, loader;
+
+function createWalls() {
+    //loader = new THREE.TextureLoader();
+    block_material = Physijs.createMaterial(
+        new THREE.MeshBasicMaterial({ color: 0xffffff }),
+        0.4, // friction
+        0.6 // restitution
+    );
+
+    createWall1();
+
+    createWall2();
+
+    createWall3();
+
+    createWall4();
+
+    
+}
+
+function createWall4() {
+    var arrX1= [-40, -45, -50, -55, -60];
+    var size = arrX1.length;
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
+            createBlock(5, 90, 3 + i * 5, arrX1[j]);        
+        } 
+    }
+
+    console.log("Create Wall 1");
+}
+function createWall3() {
+    var arrX1= [70, 76, 82, 88, 94];
+    var size = arrX1.length;
+    for (var i = 0; i < size; i++) {
+        createBlock(5, 50, 3, arrX1[i]);        
+    }
+    size--;
+
+    for (var i = 0; i < size; i++) {
+        createBlock(5, 50, 8, arrX1[i] + 2.5);
+    }
+    size--;
+
+    for (var i = 0; i < size; i++) {
+        createBlock(5, 50, 13, arrX1[i] + 5);
+    }
+    size--;
+    for (var i = 0; i < size; i++) {
+        createBlock(5, 50, 18, arrX1[i] + 7.5);
+    }
+    size--;
+
+    for (var i = 0; i < size; i++) {
+        createBlock(5, 50, 23, arrX1[i] + 10);
+    }
+
+    console.log("Create Wall 3");
+}
+function createWall2(){ 
+    var arrX = [-45,-50,-55,-60,-65];
+    for(var i = 0; i < arrX.length; i++){
+        createBlock(5, -10, 3, arrX[i]);
+    }    
+
+    var arrX2 = [-45,-50,-55,-60];
+    for(var i = 0; i < arrX2.length; i++){
+        createBlock(5, -10, 8, arrX[i] - 2.5);
+        }
+    var arrX2 = [-45,-50,-55];
+    for(var i = 0; i < arrX2.length; i++){
+        createBlock(5, -10, 13, arrX[i] - 2.5);
+        }
+
+        var arrX3 = [-45,-50];
+    for(var i = 0; i < arrX3.length; i++){
+        createBlock(5, -10, 18, arrX[i] - 2.5);
+        }
+        var arrX3 = [-45];
+    for(var i = 0; i < arrX3.length; i++){
+        createBlock(5, -10,23, arrX[i] - 2.5);
+        }
+}
+    
+function createWall1() {
+    var arrX1= [-40, -46, -52, -58, -64];
+    var size = arrX1.length;
+    for (var i = 0; i < size; i++) {
+        createBlock(5, 40, 3, arrX1[i]);        
+    }
+    size--;
+
+    for (var i = 0; i < size; i++) {
+        createBlock(5, 40, 8, arrX1[i] - 2.5);
+    }
+    size--;
+
+    for (var i = 0; i < size; i++) {
+        createBlock(5, 40, 13, arrX1[i] - 5);
+    }
+    size--;
+
+    for (var i = 0; i < size; i++) {
+        createBlock(5, 40, 18, arrX1[i] - 7.5);
+    }
+    size--;
+
+    for (var i = 0; i < size; i++) {
+        createBlock(5, 40, 23, arrX1[i] - 10);
+    }
+
+    console.log("Create Wall 1");
+}
+
+var count = 0;
+
+function createBlock(size, x, y, z) {
+    var block = new Physijs.BoxMesh(
+        new THREE.BoxGeometry(size, size, size),
+        block_material
+    );
+    block.castShadow = block.receiveShadow = true;
+    block.position.set(x, y, z);
+    block.name = "block" + count;
+    count++;
+    scene.add(block);
+}
+
+function delete3DOBJ(objName) {
+    var selectedObject = scene.getObjectByName(objName);
+    scene.remove(selectedObject);
+}
+function clearWall() {
+    for (var i = 0; i < count; i++){
+        let block_name = "block" + i;
+        delete3DOBJ(block_name);
+    }
+    count = 0;
+}
+function resetWall() {
+    console.log("Reset Wall");
+    clearWall();
+    createWalls();
+}
