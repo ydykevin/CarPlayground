93a161da661db5150096d285130f43d6a71ac3fb
wuschelbueb <Wuschelbueb@users.noreply.github.com>
2020-06-20 17:16:33 +1000

    code refactoring for light.js

--- src/light.js
+++ src/light.js
@@ -3,12 +3,12 @@
 Physijs.scripts.worker = "js/physijs_worker.js";
 Physijs.scripts.ammo = "ammo.js";
 
-var light;
+var light, softLight;
 
 function createLight() {
-    light = new THREE.DirectionalLight( 0xFFFFFF );
-    light.position.set( 200, 150, -200 );
-    light.target.position.copy( scene.position );
+    light = new THREE.DirectionalLight(0xffffff);
+    light.position.set(200, 150, -200);
+    light.target.position.copy(scene.position);
     light.castShadow = true;
     light.shadowCameraLeft = -300;
     light.shadowCameraTop = -300;
@@ -16,11 +16,11 @@ function createLight() {
     light.shadowCameraBottom = 300;
     light.shadowCameraNear = 20;
     light.shadowCameraFar = 500;
-    light.shadowBias = -.0001
+    light.shadowBias = -0.0001;
     light.shadowMapWidth = light.shadowMapHeight = 2048;
-    light.shadowDarkness = .8;
-    light.shadowCameraVisible = true
-    scene.add( light );
-    light = new THREE.AmbientLight( 0xaaaaaa,20 ); // soft white light
-    scene.add( light );
+    light.shadowDarkness = 0.8;
+    light.shadowCameraVisible = true;
+    scene.add(light);
+    softLight = new THREE.AmbientLight(0xaaaaaa, 20); // soft white light
+    scene.add(softLight);
 }
